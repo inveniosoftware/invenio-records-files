27a3e5481e7eb255268cc513d163cce3f1e5a7bc
Glignos <glignos93@gmail.com>
2019-05-13 15:30:22 +0200

    setup: fix pytest version
     Previous pytest lowest is introducing attrs==17.2.0
     Which is breaking jsonschema which requires: attrs>=17.4.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
     'pydocstyle>=1.0.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
-    'pytest>=3.3.1',
+    'pytest>=3.5.0',
     'invenio-indexer>=1.0.0',
     'invenio-search[elasticsearch6]>=1.0.0',
 ]
