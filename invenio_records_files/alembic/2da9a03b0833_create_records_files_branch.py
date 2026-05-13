1f58d4ca5c067ac5d2554398e4174bcc7767bb41
Mirek Simek <miroslav.simek@cesnet.cz>
2026-05-13 10:56:09 +0200

    fix: bad downstream dependency in alembic
    * The initial migration was generated with invenio-db as a downstream,
      which creates multiple "default" heads, disrupting alembic revision
    * The fix is to use plain depends_on instead of downstream revision
    * Tests for upgrade/downgrade added

--- invenio_records_files/alembic/2da9a03b0833_create_records_files_branch.py
+++ invenio_records_files/alembic/2da9a03b0833_create_records_files_branch.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2016-2019 CERN.
+# Copyright (C) 2026 CESNET z.s.p.o.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -13,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "2da9a03b0833"
-down_revision = "dbdbc1b19cf2"
+down_revision = None
 branch_labels = ("invenio_records_files",)
 depends_on = "dbdbc1b19cf2"
 
